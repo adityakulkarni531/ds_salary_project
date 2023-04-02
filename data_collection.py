4ea5c6de29e9a03a67fbbd1e462e71f56074fc4d
adityakulkarni531 <adityakulkarni531@gmail.com>
2023-04-02 15:03:52 +0530

    initial commit:

---
+++ data_collection.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 14:46:39 2023
+
+@author: adity
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "G:/Project/ds_salary_proj/chromedriver"
+
+df = gs.get_jobs('data scientist', 15, False, path, 15)
